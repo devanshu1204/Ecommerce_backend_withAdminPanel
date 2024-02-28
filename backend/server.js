ba79dbbe7806be986f481a552be7c95621c3c033
Devanshu Agarwal <agarwal.devanshu12@gmail.com>
2024-02-28 23:58:31 +0530

    updatedPackages.json_addedconfig.env

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello Devanshu");
+import { Express } from "express";
+import { DotenvConfigOptions } from "dotenv";
+
+const app = express();
+
+// config
+dotenv.config({ path: "backend/config/config.env" });
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on http://localhost:${process.env.PORT}`);
+});
