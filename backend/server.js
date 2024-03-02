5b8a2c46c16c29d52f74489edd56b274aa129618
Devanshu Agarwal <agarwal.devanshu12@gmail.com>
2024-03-03 02:37:45 +0530

    added gemstones data, connected mongo URI, .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import { Express } from "express";
-import { DotenvConfigOptions } from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
@@ -10,6 +10,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+// listening to the server
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on http://localhost:${process.env.PORT}`);
 });
